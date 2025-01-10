b2e4afb01744990135700b5781b0bd2b9312bfa6
zenx331 <168805831+zenx331@users.noreply.github.com>
2025-01-10 21:18:45 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,8 +1,8 @@
-global.bankname = "MONIEPOINT"
+global.bankname = "ᴀᴅᴜʟ-xᴇʀ💗🕊️"
 
-global.banknumber = "8133729715"
+global.banknumber = "9074403240"
 
-global.bankowner = "FADARE"
+global.bankowner = "ᴀᴅᴜʟ-xᴇʀ💗🕊️"
 
 global.autoTyping = false //auto tying in gc (true to on, false to off)
 global.autoRecord = false //auto recording (true to on, false to off)
